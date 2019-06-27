6360df44ae049cee3c2ff5704745c099d1bb18dc
0531jxy <786692636@qq.com>
2019-06-27 13:56:59 +0800

    first commit

---
+++ src/views/Too.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    too
+    <div>
+    {{$route.params}}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "too",
+  components: {},
+  props: {},
+  data() {
+    return {};
+  },
+  methods: {},
+  mounted() {},
+  created() {},
+  filters: {},
+  computed: {},
+  watch: {},
+  directives: {}
+};
+</script>
+
+<style scoped lang="scss" ></style>
---
+++ src/views/giao.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    giao
+      <div>giaogiao</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "giao",
+  components: {},
+  props: {},
+  data() {
+    return {};
+  },
+  methods: {},
+  mounted() {},
+  created() {},
+  filters: {},
+  computed: {},
+  watch: {},
+  directives: {}
+};
+</script>
+
+<style scoped lang="scss"></style>
